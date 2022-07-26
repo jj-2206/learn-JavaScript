78462f1a00b7f5e327677b69fa724ec30b60c763
jj-2206 <jinju.woo2022@gmail.com>
2022-07-26 15:53:04 +0900

    feat: Create Clock setInterval() Date() padStart()

--- Application/js/clock.js
+++ Application/js/clock.js
@@ -3,11 +3,20 @@ const clock = document.querySelector('#clock');
 function getClock() {
   // JavaScript Date 객체
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+// 시계 표시 즉시 실행 후, 1초 마다 실행
 getClock();
 setInterval(getClock, 1000);
 
 // setTimeout(Function, Number) ms후에 한 번 실행
 // setInterval(Function, Number) ms마다 실행
+
+/* String.prototype.padStart()
+const padStart = '1'.padStart(3, '0');
+console.log(padStart); // 001
+*/
