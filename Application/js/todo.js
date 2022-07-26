a58bd2ede9300cf02433c6de4558b43e678ccd6a
jj-2206 <jinju.woo2022@gmail.com>
2022-07-26 17:43:14 +0900

    fix: Remove todos event.target.parentElement

--- Application/js/todo.js
+++ Application/js/todo.js
@@ -2,11 +2,23 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+// target 이벤트 특정하기 -> 부모 요소 찾기
+function deleteToDo(event) {
+  // console.log(event.target.parentElement.innerText);
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
