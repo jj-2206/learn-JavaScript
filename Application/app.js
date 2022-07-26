5a3bea2dea659d62c728b439ccaed7d18b4ed502
jj-2206 <jinju.woo2022@gmail.com>
2022-07-26 13:50:07 +0900

    fix: a tag addEventListener

--- Application/app.js
+++ Application/app.js
@@ -1,5 +1,6 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const link = document.querySelector('a');
 
 // form을 submit하는 것에 주의! - html확인 form안의 input!
 function onLoginSubmit(event) {
@@ -8,4 +9,10 @@ function onLoginSubmit(event) {
 }
 loginForm.addEventListener('submit', onLoginSubmit);
 
-// -> Events part Two
+// addEventListener에 들어간 함수는 브라우저가 실행시켜주는 함수!
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+  alert('click');
+}
+link.addEventListener('click', handleLinkClick);
