b8ba0d364d5114aaed8aae77fc76f4cb8b494bb5
jj-2206 <jinju.woo2022@gmail.com>
2022-07-27 02:24:37 +0900

    fix: promise to async

--- ASYNC/callbackToPromise.js
+++ ASYNC/callbackToPromise.js
@@ -31,6 +31,7 @@ class UserStorage {
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
+
 userStorage
   .loginUser(id, password)
   .then(userStorage.getRoles)
@@ -65,13 +66,18 @@ class UserStorage {
       }, 1000);
     });
   }
+
+  async getUserWithRole(user, password) {
+    const id = await this.loginUser(user, password);
+    const role = await this.getRoles(id);
+    return role;
+  }
 }
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
 userStorage
-  .loginUser(id, password)
-  .then(userStorage.getRoles)
-  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`))
-  .catch(console.log);
+  .getUserWithRole(id, password) //
+  .catch(console.log)
+  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`));
