f3862208ceb4fd8f0214c544543abdbca782a9f6
jj-2206 <jinju.woo2022@gmail.com>
2022-07-27 13:54:13 +0900

    fix: Add JSON.stringify

--- Application/js/todo.js
+++ Application/js/todo.js
@@ -2,12 +2,14 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
-// input 입력값을 배열로 만들기
+// input 입력값을 배열로 만들기 toDos는 빈배열
 const toDos = [];
 
-// localStorage에 저장, 3:00
+// localStorage에 저장
 function saveToDos() {
-  localStorage.setItem('todos', toDos);
+  // JSON.stringify() 배열[]이나 객체{}를 string으로 만들어준다.
+  // localStorage.setItem('todos', toDos); 1,2,3
+  localStorage.setItem('todos', JSON.stringify(toDos));
 }
 
 // target 이벤트 특정하기 -> 부모 요소 찾기
@@ -38,5 +40,6 @@ function handleTodoSubmit(event) {
   toDos.push(newTodo);
   // console.log(toDos);
   paintToDo(newTodo);
+  saveToDos();
 }
 toDoForm.addEventListener('submit', handleTodoSubmit);
