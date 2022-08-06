d6fc3608006aec1e7c130c7abe5a06d8a3e5c1c6
jj-2206 <jinju.woo2022@gmail.com>
2022-08-06 14:01:16 +0900

    fix: Answer challenge day5

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const colors = ['#1abc9c', '#3498db', '#9b59b6', '#f39c12', '#e74c3c'];
+const body = document.body;
 
-function handleWindowResize() {
-  if (window.innerWidth < 400) {
-    document.body.style.backgroundColor = colors[1];
-  } else if (window.innerWidth >= 400 && window.innerWidth < 600) {
-    document.body.style.backgroundColor = colors[2];
-  } else document.body.style.backgroundColor = colors[3];
+const BIG_SCREEN = 'bigScreen';
+const MEDIUM_SCREEN = 'mediumScreen';
+const SMALL_SCREEN = 'smallScreen';
+
+function handleResize() {
+  const width = window.innerWidth;
+  if (width > 1000) {
+    body.classList.add(BIG_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  } else if (width <= 1140 && width >= 700) {
+    body.classList.add(MEDIUM_SCREEN);
+    body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
+  } else {
+    body.classList.add(SMALL_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  }
 }
-window.addEventListener('resize', handleWindowResize);
-handleWindowResize();
+
+window.addEventListener('resize', handleResize);
