b094f25a0006c2eea5bf5a028088d4b8b8a6ed00
jj-2206 <jinju.woo2022@gmail.com>
2022-08-10 01:48:02 +0900

    feat: Answered day07-08

--- nomadcoder-challenge/main.js
+++ nomadcoder-challenge/main.js
@@ -1,21 +1,40 @@
-const body = document.body;
+const setNumberForm = document.querySelector('#setNumber');
+const setNumberInput = document.querySelector('#setNumber input');
+const userNumberForm = document.querySelector('#userNumber');
+const userNumberInput = document.querySelector('#userNumber input');
+const userNumberBtn = document.querySelector('#userNumber button');
+const displayResult = document.querySelector('#displayResult');
 
-const BIG_SCREEN = 'bigScreen';
-const MEDIUM_SCREEN = 'mediumScreen';
-const SMALL_SCREEN = 'smallScreen';
+function numberSubmit(event) {
+  event.preventDefault();
+  const setNum = Number(setNumberInput.value);
+  // console.log(setNum, typeof setNum);
+}
+setNumberForm.addEventListener('submit', numberSubmit);
 
-function handleResize() {
-  const width = window.innerWidth;
-  if (width > 1000) {
-    body.classList.add(BIG_SCREEN);
-    body.classList.remove(MEDIUM_SCREEN);
-  } else if (width <= 1140 && width >= 700) {
-    body.classList.add(MEDIUM_SCREEN);
-    body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
-  } else {
-    body.classList.add(SMALL_SCREEN);
-    body.classList.remove(MEDIUM_SCREEN);
-  }
+function userNumberSubmit(event) {
+  event.preventDefault();
+  const userNum = Number(userNumberInput.value);
+  display(userNum);
+  // console.log(userNum, typeof userNum);
+}
+userNumberForm.addEventListener('submit', userNumberSubmit);
+
+function display() {
+  const userNum = Number(userNumberInput.value);
+  const setNum = Number(setNumberInput.value);
+  const randomNum = Math.floor(Math.random() * setNum + 1);
+  if (userNum === randomNum) {
+    displayResult.innerText = `you chose: ${userNum}, the machine chose: ${randomNum}.
+    \n You won!`;
+  } else
+    displayResult.innerText = `you chose: ${userNum}, the machine chose: ${randomNum}.
+  \n You lost!`;
 }
 
-window.addEventListener('resize', handleResize);
+// function makeRandom() {
+//   const setNum = Number(setNumberInput.value);
+//   const randomNum = Math.floor(Math.random() * setNum + 1);
+//   // console.log(randomNum, typeof randomNum);
+// }
+// userNumberBtn.addEventListener('click', makeRandom);
