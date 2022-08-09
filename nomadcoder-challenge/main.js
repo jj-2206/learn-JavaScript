9a76e5820c607683417e8b2004e768582b3d20c9
jj-2206 <jinju.woo2022@gmail.com>
2022-08-10 00:27:22 +0900

    feat: Answered day05
    classList 추가 삭제 작성방법
    window.innerWidth

--- nomadcoder-challenge/main.js
+++ nomadcoder-challenge/main.js
@@ -1,28 +1,21 @@
-// <⚠️ /DONT DELETE THIS ⚠️>
-const colors = ['#1abc9c', '#3498db', '#9b59b6', '#f39c12', '#e74c3c'];
-// <⚠️ /DONT DELETE THIS ⚠️>
+const body = document.body;
 
-const h2 = document.querySelector('h2');
-const superEventHandler = {
-  handleEnter: function () {
-    h2.innerText = 'The mouse is here!';
-    h2.style.color = colors[0];
-  },
-  handleLeave: function () {
-    h2.innerText = 'The mouse is gone!';
-    h2.style.color = colors[1];
-  },
-  handleResize: function () {
-    h2.innerText = 'You just resized!';
-    h2.style.color = colors[2];
-  },
-  handleClick: function () {
-    h2.innerText = 'That was a right click!';
-    h2.style.color = colors[4];
-  },
-};
+const BIG_SCREEN = 'bigScreen';
+const MEDIUM_SCREEN = 'mediumScreen';
+const SMALL_SCREEN = 'smallScreen';
 
-h2.addEventListener('mouseenter', superEventHandler.handleEnter);
-h2.addEventListener('mouseleave', superEventHandler.handleLeave);
-window.addEventListener('resize', superEventHandler.handleResize);
-window.addEventListener('contextmenu', superEventHandler.handleClick);
+function handleResize() {
+  const width = window.innerWidth;
+  if (width > 1000) {
+    body.classList.add(BIG_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  } else if (width <= 1140 && width >= 700) {
+    body.classList.add(MEDIUM_SCREEN);
+    body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
+  } else {
+    body.classList.add(SMALL_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  }
+}
+
+window.addEventListener('resize', handleResize);
